5c69aa100adcaa0483f42a943720efce7a6c217f
Emmanuel Lopez <eelopez29@gmail.com>
2022-03-24 14:29:44 -0300

    Agrego tp4-request

--- tp3-fracciones.py
+++ tp3-fracciones.py
@@ -1,38 +1,3 @@
-"""
-# Primera version sin __add__
-
-class Fraccion:
-
-    def __init__(self, numerador, denominador):
-        self.num = numerador
-        self.den = denominador
-
-    def __str__(self):
-        return f'{self.num}/{self.den}'
-    
-    def __add__(self, otro):
-        if type(otro) != Fraccion:
-            raise ValueError('Solo se puede sumar un Fraccion')
-
-
-def suma(fr1, fr2):
-    if fr1.den != fr2.den:
-        nuevoDen = fr1.den * fr2.den
-        nuevoNum = fr1.num * fr2.den + fr1.den * fr2.num
-    else:
-        nuevoNum = fr1.num + fr2.num
-        nuevoDen = fr1.den
-    return Fraccion(nuevoNum, nuevoDen)
-
-fraccion1 = Fraccion(4,2)
-fraccion2 = Fraccion(2,9)
-resultado = suma(fraccion1, fraccion2)
-
-print(resultado)
-"""
-
-# Segunda version con __add__
-
 class Fraccion:
 
     def __init__(self, numerador, denominador):
